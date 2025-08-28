64e6fe21b200df7bb2653a156be48c6c8a36ead0
traveltime07-hash <traveltime07@gmail.com>
2025-08-28 19:55:51 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import App from "./App.tsx"; // to jest Twoja obecna aplikacja (obiekty/role)
+import Landing from "./pages/Landing.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Landing jako strona główna */}
+        <Route path="/" element={<Landing />} />
+        {/* Dotychczasowa aplikacja */}
+        <Route path="/app" element={<App />} />
+        {/* Fallback na / */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
