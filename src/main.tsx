8ef66651c29c38d6252ab6c1f447cbd1ae16fa73
traveltime07-hash <traveltime07@gmail.com>
2025-08-29 17:19:05 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import App from "./App"; // Twoja obecna aplikacja (np. /app)
-import Auth from "./pages/Auth";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        {/* Domyślnie kierujemy na logowanie */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* Auth */}
-        <Route path="/login" element={<Auth defaultTab="login" />} />
-        <Route path="/rejestracja" element={<Auth defaultTab="register" />} />
-
-        {/* Twoja aplikacja (to co już masz w App.tsx) */}
-        <Route path="/app/*" element={<App />} />
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
